d28c7f68de34b4d1a383f63388f1ed98a552b470
unknown <saritalaxmi96@gmail.com>
2023-08-08 07:07:58 +0530

    modified nodejs code with async method

--- target.js
+++ target.js
@@ -1,8 +1,9 @@
 var fs = require("fs");
 
-var data = fs.readFileSync('textfile.txt');
-
+var data = fs.readFile('textfile.txt',function(err,data){
+if(err) return console.error(err);
 console.log(data.toString());
+});
 
 console.log("===================================");
 
